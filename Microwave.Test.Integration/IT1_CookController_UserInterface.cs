babf1772c4987a23303b0b4cd3f5451f6d179c27
madsnk <mads.nielsen.kjaersgaard@gmail.com>
2018-03-13 16:17:46 +0100

    started on IT step 1

---
+++ Microwave.Test.Integration/IT1_CookController_UserInterface.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT1_CookController_UserInterface
+    {
+        private CookController _uut_cooker;
+        private UserInterface _uut_ui;
+
+        private ITimer _timer;
+        private IDisplay _display;
+        private IPowerTube _powerTube;
+
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _startCancelButton;
+
+        private IDoor _door;
+        private ILight _light;
+
+        [SetUp]
+        public void Setup()
+        {
+            _powerButton = Substitute.For<IButton>();
+            _timeButton = Substitute.For<IButton>();
+            _startCancelButton = Substitute.For<IButton>();
+            _door = Substitute.For<IDoor>();
+            _light = Substitute.For<ILight>();
+            _display = Substitute.For<IDisplay>();
+
+            _timer = Substitute.For<ITimer>();
+            _powerTube = Substitute.For<IPowerTube>();
+
+            _uut_ui = new UserInterface(
+                _powerButton, _timeButton, _startCancelButton,
+                _door,
+                _display,
+                _light,
+                _uut_cooker);
+
+            _uut_cooker = new CookController(_timer, _display, _powerTube, _uut_ui);
+        }
+
+        [Test]
+        public void OnStartCancelPressed_StateIsSetTime_CookControllerStartsCooking()
+        {
+            // "Press" powerbutton --> raise event --> set state to SETPOWER for the UserInterface uut
+            // Is this the correct way to drive the uut? We are using the mocks to drive right now!?
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            _startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            // Is this OK? Or how else can we Assert that StartCooking is called on CookController
+            _powerTube.Received(1).TurnOn(50);
+        }
+    }
+}
